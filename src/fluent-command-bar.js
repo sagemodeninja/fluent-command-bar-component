b4c7b2bb2992101480229902ed9eea15e0fef35a
Gary Antier <contact@garyantier.com>
2022-05-18 11:09:44 +0800

    feat: primary commands

--- src/fluent-command-bar.js
+++ src/fluent-command-bar.js
@@ -1,3 +1,5 @@
+//box-shadow: 0 0 2px rgba(0, 0, 0, 0.2), 0 calc(32 * 0.5px) calc((32 * 1px)) rgba(0, 0, 0, 0.24);
+
 (function() {
     const template = document.createElement("template");
     template.innerHTML = `
@@ -29,11 +31,12 @@
     
     .button:active {
         background-color: rgba(160, 160, 160, 0.06);
+        color: rgba(27, 27, 27, 0.49) !important;
     }
     
     .button fluent-symbol-icon,
     .button ::slotted(*) {
-        margin-right: 5px;
+        margin-right: 8px;
     }
     
     /* Content */
@@ -132,4 +135,159 @@
     }
 
     customElements.define("fluent-app-bar-button", FluentAppBarButton);
+})();
+
+(function() {
+    const template = document.createElement("template");
+    template.innerHTML = `
+    <style>
+    :host {
+        background-color: #e5e5e5;
+        display: block;
+        height: 30px;
+        min-height: 30px;
+        width: 1px;
+    }
+    </style>
+    `;
+
+    class FluentAppBarSeparator extends HTMLElement {
+        constructor() {
+            super();
+
+            this.attachShadow({ mode: "open" });
+            this.shadowRoot.append(template.content.cloneNode(true));
+        }
+    }
+
+    customElements.define("fluent-app-bar-separator", FluentAppBarSeparator);
+})();
+
+(function() {
+    const template = document.createElement("template");
+    template.innerHTML = `
+    <style>
+    :host {
+        display: inline-block;
+        user-select: none;
+    }
+
+    .command-bar {
+        border-radius: 4px;
+        box-sizing: border-box;
+        padding: 6px 1px 6px 6px;
+        position: relative;
+    }
+
+    .command-bar.active {
+        background-color: #fff;
+        border: solid 1px #ebebeb;
+        padding: 5px;
+    }
+
+    .command-bar,
+    .primary-commands {
+        align-items: center;
+        column-gap: 5px;
+        display: flex;
+    }
+
+    /* Button */
+    .button {
+        align-items: center;
+        background-color: transparent;
+        border-radius: 5px;
+        box-sizing: border-box;
+        color: #1b1b1b;
+        cursor: default;
+        display: flex;
+        height: 36px;
+        min-height: 36px;
+        padding: 0 3px;
+        position: relative;
+    }
+    
+    .button:hover {
+        background-color: rgba(156, 156, 156, 0.1);
+    }
+    
+    .button:active {
+        background-color: rgba(160, 160, 160, 0.06);
+        color: rgba(27, 27, 27, 0.49) !important;
+    }
+    
+    .button fluent-symbol-icon {
+        margin: 0 8px;
+    }
+    </style>
+    <div class='command-bar'>
+        <div class='primary-commands'>
+            <slot></slot>
+        </div>
+        <div class='button more-button'>
+            <fluent-symbol-icon symbol='More' font-size="20" title="See more"></fluent-symbol-icon>
+        </div>
+    </div>
+    `;
+
+    class FluentCommandBar extends HTMLElement {
+        constructor() {
+            super();
+
+            this.attachShadow({ mode: "open" });
+            this.shadowRoot.append(template.content.cloneNode(true));
+        }
+
+        static get observedAttributes() {
+            return ["is-open"];
+        }
+
+        /* Attributes */
+        get isOpen() {
+            return this.getAttribute("is-open");
+        }
+
+        set isOpen(value) {
+            this.setAttribute("is-open", value);
+            this.setIsOpen();
+        }
+        
+        /* DOM */
+        get commandBar() {
+            this._commandBar ??= this.shadowRoot.querySelector(".command-bar");
+            return this._commandBar;
+        }
+        
+        get moreButton() {
+            this._moreButton ??= this.shadowRoot.querySelector(".more-button");
+            return this._moreButton;
+        }
+
+        connectedCallback() {
+            this.setIsOpen();
+
+            // Event listeners
+            this.moreButton.addEventListener("click", e => {
+                this.toggleAttribute("is-open");
+                e.stopPropagation();
+            });
+
+            // Close 
+            window.addEventListener("click", () => {
+                this.toggleAttribute("is-open", false);
+            });
+        }
+        
+        attributeChangedCallback(name, oldValue, newValue) {
+            switch (name) {
+                case "is-open": this.setIsOpen(); break;
+            }
+        }
+
+        setIsOpen() {
+            this.commandBar.classList.toggle("active", eval(this.isOpen));
+        }
+    }
+
+    customElements.define("fluent-command-bar", FluentCommandBar);
 })();
